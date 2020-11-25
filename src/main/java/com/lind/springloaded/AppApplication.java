2ab92494869440ce6e2c345473f7331fa8386ebf
bfyxzls <bfyxzls@sina.com>
2020-11-25 22:52:50 +0800

    add jar loader

--- src/main/java/com/lind/springloaded/AppApplication.java
+++ src/main/java/com/lind/springloaded/AppApplication.java
@@ -1,14 +1,13 @@
 package com.lind.springloaded;
 
-import java.util.concurrent.TimeUnit;
+import com.lind.springloaded.utils.JarLoader;
+
+import java.util.Map;
 
 public class AppApplication {
     public static void main(String[] args) throws Exception {
-        while (true) {
-            Reload reload = new Reload();
-            reload.load();
-            TimeUnit.SECONDS.sleep(3);
-        }
+        Map<String, Class<?>> dll = JarLoader.loadJarFromAbsolute("/Users/zzl/xboot-fast-1.0-SNAPSHOT.jar");
+        dll.forEach((i, o) -> System.out.println(i));
     }
 
 
